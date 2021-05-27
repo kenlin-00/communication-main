fc62a8ee7992876318953a56588b5232bc02dd9c
kendall-cpp <1456263654@qq.com>
2021-05-27 11:50:53 +0800

    实现日志模块

---
+++ app/ngx_log.cxx
@@ -0,0 +1,208 @@
+﻿//和日志相关的函数放之类
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>    //uintptr_t
+#include <stdarg.h>    //va_start....
+#include <unistd.h>    //STDERR_FILENO等
+#include <sys/time.h>  //gettimeofday
+#include <time.h>      //localtime_r
+#include <fcntl.h>     //open
+#include <errno.h>     //errno
+
+#include "ngx_global.h"
+#include "ngx_macro.h"
+#include "ngx_func.h"
+#include "ngx_c_conf.h"
+
+//全局量---------------------
+//错误等级，和ngx_macro.h里定义的日志等级宏是一一对应关系
+static u_char err_levels[][20]  = 
+{
+    {"stderr"},    //0：控制台错误
+    {"emerg"},     //1：紧急
+    {"alert"},     //2：警戒
+    {"crit"},      //3：严重
+    {"error"},     //4：错误
+    {"warn"},      //5：警告
+    {"notice"},    //6：注意
+    {"info"},      //7：信息
+    {"debug"}      //8：调试
+};
+ngx_log_t   ngx_log;
+
+void ngx_log_stderr(int err, const char *fmt, ...)
+{    
+    va_list args;                        //创建一个va_list类型变量
+    u_char  errstr[NGX_MAX_ERROR_STR+1]; //2048  -- ************  +1是我自己填的，感谢官方写法有点小瑕疵，所以动手调整一下
+    u_char  *p,*last;
+
+    memset(errstr,0,sizeof(errstr));     
+    last = errstr + NGX_MAX_ERROR_STR;          
+    p = ngx_cpymem(errstr, "nginx: ", 7);     //p指向"nginx: "之后    
+    
+    va_start(args, fmt); //使args指向起始的参数
+    p = ngx_vslprintf(p,last,fmt,args); //组合出这个字符串保存在errstr里
+    va_end(args);        //释放args
+
+    if (err)  //如果错误代码不是0，表示有错误发生
+    {
+        //错误代码和错误信息也要显示出来
+        p = ngx_log_errno(p, last, err);
+    }
+    
+    //若位置不够，那换行也要硬插入到末尾，哪怕覆盖到其他内容
+    if (p >= (last - 1))
+    {
+        p = (last - 1) - 1; 
+    }
+    *p++ = '\n'; //增加个换行符    
+
+    //往标准错误【一般是屏幕】输出信息    
+    write(STDERR_FILENO,errstr,p - errstr); //三章七节讲过，这个叫标准错误，一般指屏幕
+
+    //测试代码：
+    //printf("ngx_log_stderr()处理结果=%s\n",errstr);
+    //printf("ngx_log_stderr()处理结果=%s",errstr);
+    
+    return;
+}
+
+
+u_char *ngx_log_errno(u_char *buf, u_char *last, int err)
+{
+    //以下代码是我自己改造，感觉作者的代码有些瑕疵
+    char *perrorinfo = strerror(err); //根据资料不会返回NULL;
+    size_t len = strlen(perrorinfo);
+
+    //然后我还要插入一些字符串： (%d:)  
+    char leftstr[10] = {0}; 
+    sprintf(leftstr," (%d: ",err);
+    size_t leftlen = strlen(leftstr);
+
+    char rightstr[] = ") "; 
+    size_t rightlen = strlen(rightstr);
+    
+    size_t extralen = leftlen + rightlen; //左右的额外宽度
+    if ((buf + len + extralen) < last)
+    {
+        //保证整个我装得下，我就装，否则我全部抛弃 ,nginx的做法是 如果位置不够，就硬留出50个位置【哪怕覆盖掉以往的有效内容】，也要硬往后边塞，这样当然也可以；
+        buf = ngx_cpymem(buf, leftstr, leftlen);
+        buf = ngx_cpymem(buf, perrorinfo, len);
+        buf = ngx_cpymem(buf, rightstr, rightlen);
+    }
+    return buf;
+}
+
+void ngx_log_error_core(int level,  int err, const char *fmt, ...)
+{
+    u_char  *last;
+    u_char  errstr[NGX_MAX_ERROR_STR+1];   //这个+1也是我放入进来的，本函数可以参考ngx_log_stderr()函数的写法；
+
+    memset(errstr,0,sizeof(errstr));  
+    last = errstr + NGX_MAX_ERROR_STR;   
+    
+    struct timeval   tv;
+    struct tm        tm;
+    time_t           sec;   //秒
+    u_char           *p;    //指向当前要拷贝数据到其中的内存位置
+    va_list          args;
+
+    memset(&tv,0,sizeof(struct timeval));    
+    memset(&tm,0,sizeof(struct tm));
+
+    gettimeofday(&tv, NULL);     
+
+    sec = tv.tv_sec;             //秒
+    localtime_r(&sec, &tm);      //把参数1的time_t转换为本地时间，保存到参数2中去，带_r的是线程安全的版本，尽量使用
+    tm.tm_mon++;                 //月份要调整下正常
+    tm.tm_year += 1900;          //年份要调整下才正常
+    
+    u_char strcurrtime[40]={0};  //先组合出一个当前时间字符串，格式形如：2019/01/08 19:57:11
+    ngx_slprintf(strcurrtime,  
+                    (u_char *)-1,                       
+                    "%4d/%02d/%02d %02d:%02d:%02d",     //格式是 年/月/日 时:分:秒
+                    tm.tm_year, tm.tm_mon,
+                    tm.tm_mday, tm.tm_hour,
+                    tm.tm_min, tm.tm_sec);
+    p = ngx_cpymem(errstr,strcurrtime,strlen((const char *)strcurrtime));  
+    p = ngx_slprintf(p, last, " [%s] ", err_levels[level]);                
+    p = ngx_slprintf(p, last, "%P: ",ngx_pid);                             
+
+    va_start(args, fmt);                     //使args指向起始的参数
+    p = ngx_vslprintf(p, last, fmt, args);   //把fmt和args参数弄进去，组合出来这个字符串
+    va_end(args);                            //释放args 
+
+    if (err)  //如果错误代码不是0，表示有错误发生
+    {
+        //错误代码和错误信息也要显示出来
+        p = ngx_log_errno(p, last, err);
+    }
+    //若位置不够，那换行也要硬插入到末尾，哪怕覆盖到其他内容
+    if (p >= (last - 1))
+    {
+        p = (last - 1) - 1; 
+    }
+    *p++ = '\n'; //增加个换行符       
+
+
+    ssize_t   n;
+    while(1) 
+    {        
+        if (level > ngx_log.log_level) 
+        {
+
+            break;
+        }
+
+
+        //写日志文件        
+        n = write(ngx_log.fd,errstr,p - errstr);  //文件写入成功后，如果中途
+        if (n == -1) 
+        {
+            //写失败有问题
+            if(errno == ENOSPC) //写失败，且原因是磁盘没空间了
+            {
+
+            }
+            else
+            {
+                //这是有其他错误，那么我考虑把这个错误显示到标准错误设备吧；
+                if(ngx_log.fd != STDERR_FILENO) //当前是定位到文件的，则条件成立
+                {
+                    n = write(STDERR_FILENO,errstr,p - errstr);
+                }
+            }
+        }
+        break;
+    } //end while    
+    return;
+}
+
+
+void ngx_log_init()
+{
+    u_char *plogname = NULL;
+    size_t nlen;
+
+    //从配置文件中读取和日志相关的配置信息
+    CConfig *p_config = CConfig::GetInstance();
+    plogname = (u_char *)p_config->GetString("Log");
+    if(plogname == NULL)
+    {
+        //没读到，就要给个缺省的路径文件名了
+        plogname = (u_char *) NGX_ERROR_LOG_PATH; 
+    }
+    ngx_log.log_level = p_config->GetIntDefault("LogLevel",NGX_LOG_NOTICE);
+    //nlen = strlen((const char *)plogname);
+
+
+    ngx_log.fd = open((const char *)plogname,O_WRONLY|O_APPEND|O_CREAT,0644);  
+    if (ngx_log.fd == -1)  //如果有错误，则直接定位到 标准错误上去 
+    {
+        ngx_log_stderr(errno,"[alert] could not open error log file: open() \"%s\" failed", plogname);
+        ngx_log.fd = STDERR_FILENO; //直接定位到标准错误去了        
+    } 
+    return;
+}
---
+++ app/ngx_printf.cxx
@@ -0,0 +1,288 @@
+﻿//和打印格式相关的函数放这里
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>   //类型相关头文件
+
+#include "ngx_global.h"
+#include "ngx_macro.h"
+#include "ngx_func.h"
+
+//只用于本文件的一些函数声明就放在本文件中
+static u_char *ngx_sprintf_num(u_char *buf, u_char *last, uint64_t ui64,u_char zero, uintptr_t hexadecimal, uintptr_t width);
+
+
+u_char *ngx_slprintf(u_char *buf, u_char *last, const char *fmt, ...) 
+{
+    va_list   args;
+    u_char   *p;
+
+    va_start(args, fmt); //使args指向起始的参数
+    p = ngx_vslprintf(buf, last, fmt, args);
+    va_end(args);        //释放args   
+    return p;
+}
+
+u_char *ngx_vslprintf(u_char *buf, u_char *last,const char *fmt,va_list args)
+{
+
+    u_char     zero;
+
+    /*
+    #ifdef _WIN64
+        typedef unsigned __int64  uintptr_t;
+    #else
+        typedef unsigned int uintptr_t;
+    #endif
+    */
+    uintptr_t  width,sign,hex,frac_width,scale,n;  //临时用到的一些变量
+
+    int64_t    i64;   //保存%d对应的可变参
+    uint64_t   ui64;  //保存%ud对应的可变参，临时作为%f可变参的整数部分也是可以的 
+    u_char     *p;    //保存%s对应的可变参
+    double     f;     //保存%f对应的可变参
+    uint64_t   frac;  //%f可变参数,根据%.2f等，取得小数部分的2位后的内容；
+    
+
+    while (*fmt && buf < last) //每次处理一个字符，处理的是  "invalid option: \"%s\",%d" 中的字符
+    {
+        if (*fmt == '%')  //%开头的一般都是需要被可变参数 取代的 
+        {
+            //-----------------变量初始化工作开始-----------------
+            //++fmt是先加后用，也就是fmt先往后走一个字节位置，然后再判断该位置的内容
+            zero  = (u_char) ((*++fmt == '0') ? '0' : ' ');  
+                                                                
+            width = 0;                                       
+            sign  = 1;                                      
+            hex   = 0;                                       
+            frac_width = 0;                               
+            i64 = 0;                                      
+            ui64 = 0;                                      
+            
+            //-----------------变量初始化工作结束-----------------
+
+
+            while (*fmt >= '0' && *fmt <= '9')   
+            {
+                //第一次 ：width = 1;  第二次 width = 16，所以整个width = 16；
+                width = width * 10 + (*fmt++ - '0');
+            }
+
+            for ( ;; ) //一些特殊的格式，我们做一些特殊的标记【给一些变量特殊值等等】
+            {
+                switch (*fmt)  //处理一些%之后的特殊字符
+                {
+                case 'u':      
+                    sign = 0;  
+                    fmt++;      
+                    continue;   
+
+                case 'X':       
+                    hex = 2;    
+                    sign = 0;
+                    fmt++;
+                    continue;
+                case 'x':      
+                    hex = 1;   
+                    sign = 0;
+                    fmt++;
+                    continue;
+
+                case '.':       
+                    fmt++;      
+                    while(*fmt >= '0' && *fmt <= '9')  
+                    {
+                        frac_width = frac_width * 10 + (*fmt++ - '0'); 
+                    } //end while(*fmt >= '0' && *fmt <= '9') 
+                    break;
+
+                default:
+                    break;                
+                } //end switch (*fmt) 
+                break;
+            } //end for ( ;; )
+
+            switch (*fmt) 
+            {
+            case '%':
+                *buf++ = '%';
+                fmt++;
+                continue;
+
+            case 'd':
+                if (sign)  //如果是有符号数
+                {
+                    i64 = (int64_t) va_arg(args, int);  //va_arg():遍历可变参数，var_arg的第二个参数表示遍历的这个可变的参数的类型
+                }
+                else //如何是和 %ud配合使用，则本条件就成立
+                {
+                    ui64 = (uint64_t) va_arg(args, u_int);    
+                }
+                break;  /
+
+            case 's': //一般用于显示字符串
+                p = va_arg(args, u_char *); //va_arg():遍历可变参数，var_arg的第二个参数表示遍历的这个可变的参数的类型
+
+                while (*p && buf < last)  //没遇到字符串结束标记，并且buf值够装得下这个参数
+                {
+                    *buf++ = *p++;  //那就装，比如  "%s"    ，   "abcdefg"，那abcdefg都被装进来
+                }
+                
+                fmt++;
+                continue; //重新从while开始执行 
+
+            case 'P':  //转换一个pid_t类型
+                i64 = (int64_t) va_arg(args, pid_t);
+                sign = 1;
+                break;
+
+            case 'f': 
+                f = va_arg(args, double); 
+                if (f < 0)  //负数的处理
+                {
+                    *buf++ = '-'; //单独搞个负号出来
+                    f = -f; //那这里f应该是正数了!
+                }
+                //走到这里保证f肯定 >= 0【不为负数】
+                ui64 = (int64_t) f; //正整数部分给到ui64里
+                frac = 0;
+
+                //如果要求小数点后显示多少位小数
+                if (frac_width) //如果是%d.2f，那么frac_width就会是这里的2
+                {
+                    scale = 1;  //缩放从1开始
+                    for (n = frac_width; n; n--) 
+                    {
+                        scale *= 10; //这可能溢出哦
+                    }
+
+                    frac = (uint64_t) ((f - (double) ui64) * scale + 0.5);   /
+
+                    if (frac == scale)   
+                    {
+                        ui64++;    //正整数部分进位
+                        frac = 0;  //小数部分归0
+                    }
+                } //end if (frac_width)
+
+                //正整数部分，先显示出来
+                buf = ngx_sprintf_num(buf, last, ui64, zero, 0, width); //把一个数字 比如“1234567”弄到buffer中显示
+
+                if (frac_width) //指定了显示多少位小数
+                {
+                    if (buf < last) 
+                    {
+                        *buf++ = '.'; //因为指定显示多少位小数，先把小数点增加进来
+                    }
+                    buf = ngx_sprintf_num(buf, last, frac, '0', 0, frac_width); //frac这里是小数部分，显示出来，不够的，前边填充'0'字符
+                }
+                fmt++;
+                continue;  //重新从while开始执行
+
+            //..................................
+            //................其他格式符，逐步完善
+            //..................................
+
+            default:
+                *buf++ = *fmt++; //往下移动一个字符
+                continue; 
+            } //end switch (*fmt) 
+            
+            //显示%d的，会走下来，其他走下来的格式日后逐步完善......
+
+            //统一把显示的数字都保存到 ui64 里去；
+            if (sign) //显示的是有符号数
+            {
+                if (i64 < 0)  //这可能是和%d格式对应的要显示的数字
+                {
+                    *buf++ = '-';  //小于0，自然要把负号先显示出来
+                    ui64 = (uint64_t) -i64; //变成无符号数（正数）
+                }
+                else //显示正数
+                {
+                    ui64 = (uint64_t) i64;
+                }
+            } //end if (sign) 
+
+            //把一个数字 比如“1234567”弄到buffer中显示，如果是要求10位，则前边会填充3个空格比如“   1234567”
+            //注意第5个参数hex，是否以16进制显示，比如如果你是想以16进制显示一个数字则可以%Xd或者%xd，此时hex = 2或者1
+            buf = ngx_sprintf_num(buf, last, ui64, zero, hex, width); 
+            fmt++;
+        }
+        else  //当成正常字符，源【fmt】拷贝到目标【buf】里
+        {
+            //用fmt当前指向的字符赋给buf当前指向的位置，然后buf往前走一个字符位置，fmt当前走一个字符位置
+            *buf++ = *fmt++;   //*和++优先级相同，结合性从右到左，所以先求的是buf++以及fmt++，但++是先用后加；
+        } //end if (*fmt == '%') 
+    }  //end while (*fmt && buf < last) 
+    
+    return buf;
+}
+
+
+static u_char * ngx_sprintf_num(u_char *buf, u_char *last, uint64_t ui64, u_char zero, uintptr_t hexadecimal, uintptr_t width)
+{
+    //temp[21]
+    u_char      *p, temp[NGX_INT64_LEN + 1];   //#define NGX_INT64_LEN   (sizeof("-9223372036854775808") - 1)     = 20   ，注意这里是sizeof是包括末尾的\0，不是strlen；             
+    size_t      len;
+    uint32_t    ui32;
+
+    static u_char   hex[] = "0123456789abcdef";  //跟把一个10进制数显示成16进制有关，换句话说和  %xd格式符有关，显示的16进制数中a-f小写
+    static u_char   HEX[] = "0123456789ABCDEF";  //跟把一个10进制数显示成16进制有关，换句话说和  %Xd格式符有关，显示的16进制数中A-F大写
+
+    p = temp + NGX_INT64_LEN; //NGX_INT64_LEN = 20,所以 p指向的是temp[20]那个位置，也就是数组最后一个元素位置
+
+    if (hexadecimal == 0)  
+    {
+        if (ui64 <= (uint64_t) NGX_MAX_UINT32_VALUE)   //NGX_MAX_UINT32_VALUE :最大的32位无符号数：十进制是‭4294967295‬
+        {
+            ui32 = (uint32_t) ui64; 
+            {
+                *--p = (u_char) (ui32 % 10 + '0');  
+            }
+            while (ui32 /= 10); //每次缩小10倍等于去掉屁股后边这个数字
+        }
+        else
+        {
+            do 
+            {
+                *--p = (u_char) (ui64 % 10 + '0');
+            } while (ui64 /= 10); //每次缩小10倍等于去掉屁股后边这个数字
+        }
+    }
+    else if (hexadecimal == 1)  
+    {
+        do 
+        {            
+            *--p = hex[(uint32_t) (ui64 & 0xf)];    
+        } while (ui64 >>= 4);   
+    } 
+    else // hexadecimal == 2   
+    { 
+       
+        do 
+        { 
+            *--p = HEX[(uint32_t) (ui64 & 0xf)];
+        } while (ui64 >>= 4);
+    }
+
+    len = (temp + NGX_INT64_LEN) - p;  //得到这个数字的宽度，比如 “7654321”这个数字 ,len = 7
+
+    while (len++ < width && buf < last)  
+    {
+        *buf++ = zero; 
+    }
+    
+    len = (temp + NGX_INT64_LEN) - p; 
+
+
+    if((buf + len) >= last)   
+    {
+        len = last - buf; //剩余的buf有多少我就拷贝多少
+    }
+
+    return ngx_cpymem(buf, p, len); //把最新buf返回去；
+}
+
